80f70964ab90c3d0e870d8242e32a2704f5eff40
YezeyuanZheng <146777209+YezeyuanZheng@users.noreply.github.com>
2023-11-07 22:57:48 -0500

    Add files via upload

--- web/web.py
+++ web/web.py
@@ -6,96 +6,147 @@ from datetime import datetime as dt
 from PIL import Image
 import time
 import os
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import argparse
 
-DRIVER_PATH = "C:/Program Files/Google/Chrome/Application/chrome.exe"
-options = Options()
-options.headless = True
-options.page_load_strategy = 'normal'
-options.add_argument("--window-size=1920,1200")
+#Sample Usage
+#python web.py --dp "C:/Program Files/Google/Chrome/Application/chrome.exe"
+#                  --sw truck --s "D:/saved_img/pick_up_truck/"
 
-
-pickup_truck = "https://www.google.com/search?q=pickup_truck&tbm=isch&ved=2ahUKEwiKyYfw5qiCAxUdMGIAHe73D7EQ2-cCegQIABAA&oq=pickup_truck&gs_lcp=CgNpbWcQAzIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgQIABAeMgQIABAeMgQIABAeOgoIABCKBRCxAxBDOgUIABCABDoICAAQgAQQsQM6CAgAEAgQBxAeUKUGWI0jYLwyaAJwAHgAgAGyAYgB0giSAQM1LjWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=FmpFZYrmDp3giLMP7u-_iAs&bih=704&biw=1463&rlz=1C1ONGR_enUS982US982"
-delay = 5
-max_lm = 5
+delay = 2
 
 
-driver = webdriver.Chrome(options=options)
-#driver.get(pickup_truck)
-#driver.quit()
-
-def get_im(wd,delay,max_im,url):
+def get_im(wd,delay,url):
     def scroll(driver):
         driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
         time.sleep(delay)
     url = url
     wd.get(url)
 
-    image_urls = set()
     skips = 0
+    get_end(wd)
+    thumbnails = wd.find_elements(By.CLASS_NAME, 'Q4LuWd')
+    
+    image_urls = add_img(wd,thumbnails,skips)
 
-    while len(image_urls) + skips < max_im:
-        scroll(wd)
-        thumbnails = wd.find_elements(By.CLASS_NAME, 'Q4LuWd')
-
-        for img in thumbnails[len(image_urls) + skips:max_im]:
-            try:
-                img.click()
-                time.sleep(delay)
-            except:
-                continue
-
-            images = wd.find_elements(By.XPATH, "//img[@jsname='kn3ccd']")
-            #print(images)
-            for image in images:
-                if image.get_attribute('src') in image_urls:
-                    max_lm += 1
-                    skips += 1
-                    #print("in break")
-                    break
-                if image.get_attribute('src') and 'http' in image.get_attribute('src'):
-                    image_urls.add(image.get_attribute('src'))
-                    #print("add")
-
-    #print(image_urls)
+    print(len(image_urls))
     return image_urls
+
+def add_img(wd,thumbnails,skips):
+    image_urls = set()
+    for img in thumbnails:
+        try:
+            img.click()
+            time.sleep(delay)
+        except:
+            continue
+        images = wd.find_elements(By.XPATH, "//img[@jsname='kn3ccd']")
+        #print(images)
+        for image in images:
+            if image.get_attribute('src') in image_urls:
+                #print("in break")
+                break
+            if image.get_attribute('src') and \
+                    'http' in image.get_attribute('src'):
+                image_urls.add(image.get_attribute('src'))
+                #print("add")
+    return image_urls
+    
+
+def get_end(wd):
+    scroll(wd)
+    scroll(wd)
+    scroll(wd)
+    scroll(wd)
+    scroll(wd)
+    WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                            "input[value='Show more results']"))).click()
+    scroll(wd)
+    scroll(wd)
+    scroll(wd)
+    scroll(wd)  
+
+def scroll(driver):
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+    time.sleep(delay)
 
 def download_image(path, url, file_name, image_type='PNG',verbose=True):
     try:
         time = dt.now()
         cur_time = time.strftime('%H:%M:%S')
-        image_content = requests.get(url).content
-        img_file = io.BytesIO(image_content)
-        image = Image.open(img_file)
+        image_content = requests.get(url,timeout=8).content
+        #print(url)
+        #img_file = io.BytesIO(image_content)
+        #image = Image.open(img_file)
         file_pth = path + file_name
 
         with open(file_pth, 'wb') as file:
-            image.save(file,image_type)
+            file.write(image_content)
 
         if verbose == True:
-            print(f'The image: {file_pth} downloaded successfully at {cur_time}')
+            print(f'The image: {file_pth} downloaded successful at {cur_time}')
     except Exception as e:
         print(f'download failed due to:\n: {str(e)}')
 
-if __name__ == '__main__':
-    google_url = [pickup_truck]
+def google_search(wd,searchterm):
+    page = "http://www.google.com/"
+    landing = wd.get(page)
+    WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.NAME,
+                                    "q"))).send_keys(searchterm+Keys.RETURN)
+    
+    WebDriverWait(wd, 20).until(EC.element_to_be_clickable((By.CLASS_NAME,
+                                                "LatpMc"))).click()
+    #imagesLink = wd.find_elements(By.CLASS_NAME ,"LatpMc nPDzT T3FoJb")
+    #print(imagesLink)
+    #WebDriverWait(wd, 10)
+    #WebDriverWait(wd, 2)
+    #imagesLink.click()
+    #WebDriverWait(sendsearch, '10')
+    #logo = driver.find_element_by_xpath('//*[@id="rg_s"]/div[1]/a').click()
+    #WebDriverWait(logo, '10')
+    #logolink = driver.find_element_by_xpath('//*[@id="irc_cc"]/div[3]/div[1]/div[2]/div[2]/a')
+    #WebDriverWait(logolink, '10')
+    #actions.move_to_element(logolink).click(logolink)  
 
-    labels = ['pickup_truck']
+    #print(wd.current_url)
+    return wd.current_url
 
-    assert(len(google_url)==len(labels))
 
-    save_path = 'D:/saved_img/'
-    for lb in labels:
-        if not os.path.exists(save_path + lb):
-            print(f'Make directory: {str(lb)}')
-            os.makedirs(save_path+lb)
+parser = argparse.ArgumentParser()
+parser.add_argument("--dp",
+                    help="This is thepath to you Chrome driver, driver path")
+parser.add_argument("--sw",
+                    help="This is the word that you want to search on Chrome, search words")
+parser.add_argument("--s",
+                    help="This is path of where you want to put the image")
+args = parser.parse_args()
+
+DRIVER_PATH = args.dp
+options = Options()
+options.headless = True
+options.page_load_strategy = 'normal'
+options.add_argument("--window-size=1920,1200")
+driver = webdriver.Chrome(options=options)
+
+urls_image = google_search(driver,args.sw)
+
+#'D:/saved_img/pick_up_truck'
+
+save_path = args.s
+
+if not os.path.exists(save_path):
+    print(f'Make directory: {str(save_path)}')
+    os.makedirs(save_path)
             
-    for url_cur, lb in zip(google_url,labels):
-        urls = get_im(driver,delay,max_lm,url_cur)
+urls = get_im(driver,delay,urls_image)
 
-        for i,url in enumerate(urls):
-            download_image(path=f'D:/saved_img/{lb}/',url=url,file_name=str(i+1)+'.png',verbose=True)
+for i,url in enumerate(urls):
+    download_image(path=f'{save_path}',
+                   url=url,file_name=str(i+1)+'.png',verbose=True)
         
-    driver.quit()
+driver.quit()
     
 #get_im(driver,delay,max_lm,pickup_truck)
